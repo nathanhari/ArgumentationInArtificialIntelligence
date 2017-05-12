aa978f43a5bc7ece1e8cb84ff7e62cb1b6a2caee
Hari Sarang Nathan <nathanhari@gmail.com>
2017-05-12 08:23:15 -0700

    Base Class
    Base class for argumentation frameworks. By definition, they contain two
    things: (1) arguments; and (2) attacks.

---
+++ ArgumentationFramework.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+class ArgumentationFramework:
+    def __init__(arguments, attacks):
+        self.arguments = arguments
+        self.attacks = attacks
